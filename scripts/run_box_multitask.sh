4ba503f481a5666bbc5e518ef03e3193723b4993
yixiao-huang <shawhuang22@gmail.com>
2025-11-24 00:59:32 +0000

    update multitask scripts

---
+++ scripts/run_box_multitask.sh
@@ -0,0 +1,90 @@
+
+## run nvidia-smi to check available GPUs
+export CUDA_VISIBLE_DEVICES=0
+
+## Parameters
+task_name_set=stack_cube # ['stack_cube', 'close_box']
+task_name_sets=("stack_cube" "close_box")
+random_level=0          # 0: No randomization 1: Randomize visual material 2: Randomize camera pose 3: Randomize object reflection and lighting
+num_envs=1              # Number of parallel environments
+demo_start_idx=0      # Index of the first demo to collect
+num_demo_success=100
+max_demo_idx=100    # Maximum index of demos to collect
+expert_data_num=100  # Number of expert demonstration data to convert
+sim_set=isaacsim # ['mujoco', 'isaacsim']
+cust_name=test
+retry_num=0   # Number of retries for each demo
+obs_space=joint_pos
+act_space=joint_pos
+delta_ee=0              # Delta control
+# debug_extra="-dt:0.001-decimation:10"  # Extra debug arguments
+# debug_extra="_camera"  # Extra debug arguments
+debug_extra=""  # Extra debug arguments
+extra="obs:${obs_space}_act:${act_space}${debug_extra}"
+if [ "${delta_ee}" = 1 ]; then
+  extra="${extra}_delta"
+fi
+
+cust_name="${cust_name}"
+
+# python collect_demo.py \
+#   --task CloseBox \
+#   --robot franka \
+#   --sim isaaclab \
+#   --num_envs 1 \
+#   --headless True \
+#   --random.level 2 \
+#   --run_unfinished
+
+## Collecting demonstration data
+mode=collect # collect / convert
+
+# if [ "${mode}" = "collect" ]; then
+echo "Collect demonstration data for task: ${task_name_set}"
+# python scripts/advanced/collect_demo.py \
+#   --sim=${sim_set} \
+#   --task=${task_name_set} \
+#   --num_envs=${num_envs} \
+#   --run_unfinished \
+#   --headless \
+#   --demo_start_idx=${demo_start_idx} \
+#   --num_demo_success=${num_demo_success} \
+#   --no-run_all \
+#   --retry_num=${retry_num} \
+#   --cust_name=${cust_name} \
+
+# --enable_randomization
+# elif [ "${mode}" = "convert" ]; then
+echo "Convert demonstration data for task: ${task_name_set}"
+## Convert demonstration data
+
+# python roboverse_learn/il/multi-task/append_task_config.py \
+#   --config_path roboverse_learn/il/multi-task/configs/task_configs.json \
+#   --task_name ${task_name_set}FrankaL${random_level}_${extra} \
+#   --max_demo_idx ${max_demo_idx} \
+#   --expert_data_num ${expert_data_num} \
+#   --metadata_dir roboverse_demo/demo_${sim_set}/${task_name_set}-${cust_name}/robot-franka/success \
+#   --downsample_ratio 1 \
+#   --show
+
+
+# python roboverse_learn/il/data2zarr_dp.py \
+#   --task_name ${task_name_set}FrankaL${random_level}_${extra} \
+#   --expert_data_num ${expert_data_num} \
+#   --max_demo_idx ${max_demo_idx} \
+#   --metadata_dir roboverse_demo/demo_${sim_set}/${task_name_set}-${cust_name}/robot-franka/success \
+#   --action_space ${act_space} \
+#   --observation_space ${obs_space}
+# else
+#   echo "Invalid mode"
+#   exit 1
+# fi
+
+echo "Convert multitask demonstration data"
+python roboverse_learn/il/data2zarr_dp_multitask.py \
+# python roboverse_learn/il/data2zarr_dp_multitask_gpt.py \
+  --task_config roboverse_learn/il/multi-task/configs/task_configs.json \
+  --output_name multitask_franka_gpt \
+  --observation_space joint_pos \
+  --action_space joint_pos
+
